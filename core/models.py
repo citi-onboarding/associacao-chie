b147f33b3c9d67f545585cf1efd1aa507fbd486b
Samuel Miranda <som3@cin.ufpe.br>
2019-11-19 15:55:33 -0300

    Adding model 'Quem Somos' and updating 'requirements.txt'

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from solo.models import SingletonModel
 
-# Create your models here.
+class QuemSomos (SingletonModel):
+        
+    description = models.TextField(verbose_name='Sobre nós', null=False, blank=False)
+    image = models.ImageField(verbose_name='Imagem', null = False, blank=False, upload_to='aboutUs/')
+    
+    class Meta:
+        verbose_name = 'Quem somos'
+        
+    def __str__(self):
+        return "Quem Somos"
