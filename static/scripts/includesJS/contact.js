774a5f4dbf4ebb86e5887466043c8eae9c7295dc
Samuel Miranda <som3@cin.ufpe.br>
2019-11-27 23:23:16 -0300

    updating JS from contacts section

--- static/scripts/includesJS/contact.js
+++ static/scripts/includesJS/contact.js
@@ -1,25 +1,26 @@
-// Coloque aqui os scripts relacionado a "Contatos"
-$("#contact-form").submit(function(e) {
-    e.preventDefault();
+$("#contact-content-form").submit(function(e) {
+     e.preventDefault();
 
-    const name = $('input[name = name]').val()
-    const cnpj = $('input[name = cnpj').val()
-    const email = $('input[name = email]').val()
-    const occupation= $('input[name = occupation]').val()
-    const message = $('input[name = message]').val()
+     const name = $('input[name = name]').val()
+     const cnpj = $('input[name = cnpj').val()
+     const email = $('input[name = email]').val()
+     const occupation = $('input[name = occupation]').val()
+     const message = $('textarea[name = message]').val()
 
-    const token = jQuery("[name=csrfmiddlewaretoken]").val();
+     document.getElementById('contact-content-form-reset').click();
 
-    $.ajax({
+     const token = jQuery("[name=csrfmiddlewaretoken]").val();
+
+     $.ajax({
         type: 'POST',
         url: '/contact/',
-        data: 
-            { 'csrfmiddlewaretoken': token,
-              'name' : name,
-              'cnpj' : cnpj,
-              'email' : email,
-              'occupation' : occupation,
-              'message' : message
-            },   
-    })     
-})
+        data:
+           { 'csrfmiddlewaretoken': token,
+             'name' : name,
+             'cnpj' : cnpj,
+             'email' : email,
+             'occupation' : occupation,
+             'message' : message
+            },
+   })
+ })
